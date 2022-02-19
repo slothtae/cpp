3a432253cb0e16cf4438037e1d8a16e6c71354b7
Karan Boro <68140398+slothtae@users.noreply.github.com>
2022-02-19 23:41:21 +0530

    Create reverse_array.cpp

---
+++ Lec9/reverse_array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using std::cout,std::cin,std::endl;
+
+void swap(int &a,int &b){
+    a = a+b;
+    b = a-b;
+    a = a-b;
+}
+
+void print_array(int arr[],int size){
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+}
+
+void reverse_array(int arr[],int size){
+    for(int i=0;i<size/2;i++){
+        swap(arr[i],arr[size-1-i]);
+    }
+}
+
+int main(){
+    int size,arr[100];
+    cout<<"input size : ";
+    cin>>size;
+    cout<<"Input array elements : ";
+    for(int i = 0;i<size;i++)
+        cin>>arr[i];
+    cout<<"Array:\n";
+    print_array(arr,size);
+    reverse_array(arr,size);
+    cout<<"Array after reversal :\n";
+    print_array(arr,size);
+    return 0;
+}
